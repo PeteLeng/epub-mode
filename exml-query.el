b9070a9e1dacdc027fb9c815ae335a0a68faf544
PeteLeng <pete_leng@126.com>
2024-11-03 02:44:22 +0800

    update exml query package - modify the matching behavior of wildcard patterns in a pattern list.   - patterns like '("*" nil (...)) will now try matching sibling nodes as well. - add more introduction and comments for the package. - clean up functions. - add test cases and examples.

--- exml-query.el
+++ exml-query.el
@@ -29,102 +29,131 @@
 ;; 			      t))))))
 
 (defun match-tag (n1 n2)
-  "Match the tag of two exml nodes"
+  "Match the tag of two exml nodes."
   (equal (node-tag n1) (node-tag n2)))
 
-(defun match-attr (pattr node)
-  "Match an attribute pattern against a exml node
-Each attribute pattern is a list of (op attr-name attr-val)"
-  (or (null pattr)
-      (let* ((attrs (dom-attributes node))
-	     (pname (cadr pattr))
-	     (aval (cdr (assoc pname attrs))))
-	(and aval
-	     (pcase pattr
-	       (`("=" ,_ ,pval) (string-equal pval aval))
-	       (`("*=" ,_ ,pval) (string-match pval aval))
-	       (`("^=" ,_ ,pval) (string-match (concat "^" pval) aval))
-	       (`("$=" ,_ ,pval) (string-match (concat pval "$") aval))
+(defun match-attr (patt node)
+  "Match an attribute against a exml node,
+Each pattern looks like (op name val)."
+  (or (null patt)
+      (let* ((name (cadr patt))
+	     (attrs (dom-attributes node))
+	     (val (cdr (assoc name attrs))))
+	(and val
+	     (pcase patt
+	       (`("=" ,_ ,pval) (string-equal pval val))
+	       (`("*=" ,_ ,pval) (string-match pval val))
+	       (`("^=" ,_ ,pval) (string-match (concat "^" pval) val))
+	       (`("$=" ,_ ,pval) (string-match (concat pval "$") val))
 	       (`(,op . ,_) (error "operator not implemented"))
 	       )
 	     t))))
 
-(defun match-attrs (pattrs node)
-  "Match a list of attribute pattens against a node"
-  (or (null pattrs)
-      (seq-every-p (lambda (pattr)
-		     (match-attr pattr node))
-		   pattrs)))
+(defun match-attrs (patts node)
+  "Match a list of attributes against a node."
+  (or (null patts)
+      (seq-every-p (lambda (patt)
+		     (match-attr patt node))
+		   patts)))
 
-(defun match-patt (patt root)
-  ;; patt is a node and matches from root
-  (and (consp root) ;; root cannot be a leaf node
+;; The main purpose of this package is to retrieve
+;; a list of nodes, the destination nodes, nested within
+;; the node where the search starts, the source node,
+;; where the path from the src to the dst matches the
+;; pattern.
+;; The intermediate representation for the patterns
+;; are node-like structures.
+
+;; Examples of the intermediate representation,
+;; using jQuery selectors as comparison,
+;; - All Selector ("*")
+;; - '("*" nil)
+;; - Next Adjacent Selector ("prev + next")
+;; - '("*" nil (prev nil) (next nil))
+;; - Next Siblings Selector ("prev ~ next")
+;; - '("*" nil (prev nil) (* nil) (next nil))
+;; ...
+
+;; The patterns are matched against the nodes
+;; in a DFS fashion, from top to bottom,
+;; and left to right.
+
+(defun match-patt (patt nd)
+  "Match node nd against a pattern patt,
+return the list of matched node or nodes, in case of wildcard pattern.
+patt is also a node using intermediate representation."
+  (and (not (null patt)) ;; empty pattern does not make sense.
+       (consp nd) ;; nd has to be a list, i.e., not a leaf node.
        (pcase patt
-	 (`() (list root)) ;; return root for empty pattern
-	 (`("*" . ,_) (match-wild patt root))
-	 (`(,ptag ,pattrs)
-	  (and (equal ptag (node-tag root)) ;; match pattern without children
-	       (match-attrs pattrs root)
-	       (list root)))
-	 (`(,ptag ,pattrs . ,pchildren)
-	  (and (equal ptag (node-tag root))
-	       (match-attrs pattrs root)
-	       (match-patts pchildren
-			    (node-children root))))
+	 (`() ;; should not happen, check regardless
+	  (error "empty pattern"))
+	 (`("*" . ,_) (match-wc patt nd))
+	 (`(,tag ,attrs)
+	  (and (equal tag (node-tag nd))
+	       (match-attrs attrs nd)
+	       (list nd)))
+	 (`(,tag ,attrs . ,rest)
+	  (and (equal tag (node-tag nd))
+	       (match-attrs attrs nd)
+	       (match-patts rest (node-children nd))))
 	 (_ (error "malformed pattern: %s" patt))))
   )
 
-(defun match-wild (wpat root)
-  ;; wildcard pattern is a pattern that starts with "*"
-  ;; walk down node until it potentially matches the next pattern
-  (pcase wpat
+(defun match-wc (patt nd)
+  "Match node nd with a wildcard pattern patt,
+return a list of node or nodes matched."
+  ;; wildcard pattern starts with "*"
+  (pcase patt
     (`("*" ,_)
-     (cons root
-	   (mapcan (apply-partially #'match-wild wpat)
-		   (node-children root))))
+     (cons nd
+	   (mapcan (apply-partially #'match-wc patt)
+		   (node-children nd))))
     (`("*" ,_ ,next)
-     (nconc (match-patt next root) ;; try skip *
-	    (mapcan (apply-partially #'match-wild wpat)
-		    (node-children root))))
+     (nconc (match-patt next nd) ;; skip the wc pattern
+	    (mapcan (apply-partially #'match-wc patt)
+		    (node-children nd))))
     (`("*" ,_ . ,children)
-     (nconc (mapcan (apply-partially #'match-wild wpat)
-		    (node-children root))
+     ;; traverse the node in DFS fashion
+     (nconc (mapcan (apply-partially #'match-wc patt)
+		    (node-children nd))
 	    (cl-mapcon (apply-partially #'match-patts children)
-		       (node-children root)))))
+		       (node-children nd)))))
   )
 
 (defun match-patts (patts nodes)
+  "Match a list of nodes against a list of patterns"
   ;; (message (format "match patts p: %s with n: %s" patts nodes))
-  (and nodes
+  (and (not (null patts)) ;; empty patterns do not make sense
+       nodes
        (pcase patts
-	 (`() (error "empty pat list"))
-	 (`(("*" . ,_) . ,_) (match-wilds patts nodes))
+	 (`() ;; not happening, check anyway
+	  (error "empty pattern list"))
+	 (`(("*" . ,_) . ,_) (match-patts-wc patts nodes))
+	 ;; (`(("~" . ,_) . ,rest)) ;; supported by "*"
 	 (`(,patt) (match-patt patt (car nodes)))
 	 (`(,patt . ,rest) (and (match-patt patt (car nodes))
 				(match-patts rest (cdr nodes))))))
   )
 
-(defun match-wilds (patts nodes)
+
+(defun match-patts-wc (patts nodes)
   ;; (message (format "match wilds p: %s with n: %s" patts nodes))
   (and nodes
        (pcase patts
-	 ;; not strictly necessary, but more efficient.
-	 (`(("*" ,_))
-	  (mapcan (apply-partially #'match-wild (car patts))
+	 (`(("*" ,_)) ;; branch not necessary
+	  (mapcan (apply-partially #'match-wc '("*" nil))
 		  nodes))
-	 (`(("*" ,_ . ,pchildren))
-	  (when-let ((matches (match-wild (car patts) (car nodes))))
-	    (nconc matches (match-wilds patts (cdr nodes)))
-	    ))
+	 (`(("*" ,_ . ,_))
+	  (mapcan (apply-partially #'match-wc (car patts))
+		  nodes))
 	 (`(("*" ,_) . ,rest)
-	  (nconc (match-patts rest nodes) ;; try skip *
-		 (match-wilds patts (cdr nodes))))
-	 (`(("*" ,_ . ,pchilren) . ,rest)
-	  (and (match-wild (car patts) (car nodes)) ;; match the first node
-	       (nconc (match-wilds patts (cdr nodes))
+	  (nconc (match-patts rest nodes) ;; try skip wc pattern
+		 (match-patts-wc patts (cdr nodes))))
+	 (`(("*" ,_ . ,chldr) . ,rest)
+	  (nconc (and (match-wc (car patts) (car nodes))
 		      (match-patts rest (cdr nodes)))
-	       ))))
-  )
+		 (match-patts patts (cdr nodes)))))
+       ))
 
 (defun exml-find (patt node)
   (car (match-patt patt node)))
@@ -132,9 +161,57 @@ Each attribute pattern is a list of (op attr-name attr-val)"
 (defun exml-findall (patt node)
   (match-patt patt node))
 
-;; test
+;; Tests
+;; Given the node and the following patterns,
+
+;; a
+;; | \
+;; |\ \
+;; | \ \
+;; b  c d
+;; | \ 
+;; |\ \
+;; a c d
+;;     |
+;;     e
+
+;; '("*" nil (c)) should return two nodes,
+;; '("*" nil ("*" nil (c)) (c)) should return one node.
+
+(defun exml-test1 ()
+  (let ((nd '(a nil
+		(b nil
+		   (a nil)
+		   (c nil)
+		   (d nil
+		      (e nil)))
+		(c nil)
+		(d nil)))
+	(patts '(
+		 ("*" nil (c nil))
+		 ("*" nil ("*" nil (c nil)) (c nil))
+		 ("*" nil (c nil) (d nil))
+		 ("*" nil ("*" nil (e nil)) (c nil))
+		 ("*" nil ("*" nil (e nil)) (d nil))
+		 ("*" nil ("*" nil (e nil)) ("*" nil) (d nil))
+		 )))
+    (insert (format "match node: %s\n" nd))
+    (dolist (patt patts)
+      (insert (format "with pattern: %s\n" patt))
+      (insert (format "%s\n" (match-patt patt nd))))
+    ))
+
+;; (exml-test1)
+
 (defun exml-test ()
-  (let ((node '(root nil (c1 nil (c11 nil) (c12 nil (c1 nil))) (c1 nil) (c3 nil)))
+  (let ((node '(root nil
+		     (c1 nil
+			 (c11 nil)
+			 (c12 nil
+			      (c1 nil)))
+		     (c1 nil
+			 (c12 nil))
+		     (c3 nil)))
 	(pats '(
 		(root nil)
 		(root nil (c1 nil))
@@ -153,6 +230,7 @@ Each attribute pattern is a list of (op attr-name attr-val)"
 		(root nil ("*" nil (c12 nil)) (c1 nil))
 		("*" nil (c2 nil))
 		("*" nil (c12 nil))
+		("*" nil (c3 nil))
 		)))
     (insert (format "match node: %s\n" node))
     (dolist (pat pats)
@@ -160,9 +238,6 @@ Each attribute pattern is a list of (op attr-name attr-val)"
       (insert (format "%s\n" (match-patt pat node)))))
   )
 
-;; (iter-defun range (n)
-;;   (while (> n 0)
-;;     (iter-yield n)
-;;     (setq n (1- n))))
+;; (exml-test)
 
 (provide 'exml-query)
